25209786daaf637e73ee266d3cf5444a9b5b08dc
Jake Herrmann <herrmann.jake@protonmail.com>
2024-02-19 18:30:09 -0600

    Created UI for home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleSubmission(event) {
+    event.preventDefault();
+    console.log(text);
+  }
+  
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <h1>Days Until...</h1>
+      <h2>Count down the number of days until something at a glance.</h2>
+
+      <form onSubmit={handleSubmission}>
+        <label htmlFor="nameBox" >Enter your name</label>
+        <br/>
+        <input placeholder="Name..." value={text} onChange={handleChange}/>
+      </form>
     </div>
   );
 }
+
+
 
 export default App;
